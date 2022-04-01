9806595bdab72f85613f7dac5bee089992e071c7
Brianna Lee <100378339+brileeanna06@users.noreply.github.com>
2022-04-01 11:16:05 -0500

    Add files via upload
    Files added from Chada Tech Project

---
+++ ClockFormat.h
@@ -0,0 +1,10 @@
+
+#ifndef CLOCKFORMAT_H
+#define CLOCKFORMAT_H
+
+void printNTimes(char c, int n);
+void printClock();
+void displayMenu();
+void runClock();
+
+#endif 
---
+++ MainClock.cpp
@@ -0,0 +1,7 @@
+#include "ClockFormat.h"
+
+using namespace std;
+
+int main() {
+	runClock(); //runs clock function in clockformat.cpp
+}
